c6b256448e7ee34139ebb78deaa15a89121a922e
RoanYosky <roanyosky@gmail.com>
2024-12-14 21:15:29 -0300

    Put the button on the anchor tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,12 @@ function Hero() {
                 <h2 className="header2">Roan Yosky Timane</h2>
                 <p id="developer">Web Developer</p>
                 <div className="btn-container">
+                <a href="https://www.dropbox.com/scl/fi/86556g0uvyxgscvaiphq7/CV-Roan-Yosky-Timane.pdf?rlkey=9zf4c0kxq5eyjvacbxejuu3o4&st=2q1cqi00&dl=0" 
+                    target="_blank" 
+                    download="CV-Roan-Yosky-Timane.pdf"
+                    >
                     <button className="btn btn-color-2">Download CV</button>
+                    </a>
                     <button className="btn btn-color-1"  onClick={() => window.location.href = "mailto:roanyosky@gmail.com?subject=Contact Request&body=Hello, I would like to get in touch with you!"}>Contact Me</button>
                 </div>
                 <div className="icon-container">
