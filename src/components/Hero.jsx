79faaea5cb1c0951feb24a05b3851570b170ff5e
Roan Yosky Timane <roanyosky@gmail.com>
2025-10-16 20:32:48 +0200

    Create the Hero Section for the website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,40 +1,50 @@
-import React from "react";
-import profilePicture from "../assets/images/Roan Yosky.png";
-import github from "../assets/icons/github.svg"
-import linkedin from "../assets/icons/linkedIn.svg"
+import { Github, Linkedin, Mail, ChevronDown } from 'lucide-react';
+import myAvatar from "../assets/images/Roan Yosky.png";
+import './styles/Hero.css';
 
+function Hero({ scrollToSection, darkMode }) {
+  return (
+    <section id="home" className="hero">
+      <div className="hero-container">
+        <div className="hero-content">
+          <div className="hero-avatar">
+            <img 
+              src={myAvatar}
+              alt="Roan Yosky" 
+              className="avatar-img"
+            />
+          </div>
+          <h1 className="hero-title">Roan Yosky</h1>
+          <div className="hero-divider"></div>
+          <p className="hero-subtitle">Full Stack Developer</p>
+        </div>
+        
+        <p className="hero-description">
+          Crafting elegant digital experiences through clean code and thoughtful design
+        </p>
 
+        <div className="hero-social">
+          <a href="https://github.com/RoanYoskyTimane" className="social-link">
+            <Github size={20} />
+          </a>
+          <a href="https://mz.linkedin.com/in/roan-yosky-timane-9902a0291" className="social-link">
+            <Linkedin size={20} />
+          </a>
+          <a href="mailto:roanyosky@gmail.com?subject=Contact Request&body=Hello, I would like to get in touch with you!"
+ className="social-link">
+            <Mail size={20} />
+          </a>
+        </div>
+      </div>
 
-function Hero() {
-    return (
-        <section id="profile">
-            <div className="image-section">
-                <div className="image-container">
-                    <img id="roan" src={profilePicture} alt="Roan Yosky Timane"/>
-                </div>
-            </div>
-
-            <div className="text-section">
-                <h2 className="header2">Hello,</h2>
-                <h2 className="header2">I'm</h2>
-                <h2 className="header2">Roan Yosky Timane</h2>
-                <p id="developer">Web Developer</p>
-                <div className="btn-container">
-                <a href="https://www.dropbox.com/scl/fi/86556g0uvyxgscvaiphq7/CV-Roan-Yosky-Timane.pdf?rlkey=9zf4c0kxq5eyjvacbxejuu3o4&st=2q1cqi00&dl=0" 
-                    target="_blank" 
-                    download="CV-Roan-Yosky-Timane.pdf"
-                    >
-                    <button className="btn btn-color-2">Download CV</button>
-                    </a>
-                    <button className="btn btn-color-1"  onClick={() => window.location.href = "mailto:roanyosky@gmail.com?subject=Contact Request&body=Hello, I would like to get in touch with you!"}>Contact Me</button>
-                </div>
-                <div className="icon-container">
-                    <a href="https://github.com/RoanYoskyTimane"><img src={github} alt="Github" id="github"/></a>
-                    <a href="https://mz.linkedin.com/in/roan-yosky-timane-9902a0291"><img src={linkedin} alt="LinkedIn" id="linkedin"/></a>
-                </div>
-            </div>
-        </section>
-    );
+      <button 
+        onClick={() => scrollToSection('about')}
+        className="scroll-down"
+      >
+        <ChevronDown size={32} />
+      </button>
+    </section>
+  );
 }
 
 export default Hero;
