ac9231bea94ff9149ff2f024705710c5c3ed748e
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-21 22:07:53 +0200

    Add the new icons to the skill section (oracle,c, javascript, php, nginx and linux)

--- src/data/skills.js
+++ src/data/skills.js
@@ -3,11 +3,18 @@ import vue from "../assets/icons/vue-js.svg"
 import spring from "../assets/icons/spring-boot.svg"
 import pgsql from "../assets/icons/pgsql.svg"
 import mysql from "../assets/icons/mysql.svg"
+import oracle from "../assets/icons/oracle-database.svg";
 import git from "../assets/icons/git.svg"
 import docker from "../assets/icons/docker.svg"
+import python from "../assets/icons/python.svg"
+import javascript from "../assets/icons/javascript.svg"
+import c from "../assets/icons/c-programming.svg"
+import php from "../assets/icons/php.svg"
+import nginx from "../assets/icons/nginx.svg"
+import linux from "../assets/icons/linux.svg"
+
 export const coreStack = [
   { name: 'React', icon: react  },
-  { name: 'Vue', icon: vue},
   { name: 'Spring Boot', icon: spring },
   {name: 'PostgreSQL', icon: pgsql},
   {name: 'Git', icon: git},
@@ -15,5 +22,13 @@ export const coreStack = [
 ];
 
 export const familiarWith = [
+  {name:'Linux', icon: linux},
   {name: 'MySql', icon: mysql},
+  {name: 'Oracle Database', icon: oracle},
+  {name:'Python', icon: python},
+  {name: 'Javascript', icon: javascript},
+  {name: 'C', icon: c},
+  {name: 'PHP', icon: php},
+  {name: 'Nginx', icon: nginx},
+  { name: 'Vue', icon: vue}
 ];
