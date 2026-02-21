3310a56b4a6162c0d6e73ec41bdc499d14dd2658
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-21 22:24:07 +0200

    Add target=_blank and rel=noopener noreferrer to the anchor tags for  github and linkedin on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,10 +16,10 @@ function Footer({ scrollToSection,darkMode }) {
                     © {getYear()}  Roan Yosky Timane. All rights reserved.
                 </p>
                 <div className="footer-social">
-                    <a href="https://github.com/RoanYoskyTimane" className="footer-link">
+                    <a href="https://github.com/RoanYoskyTimane" className="footer-link" target="_blank" rel="noopener noreferrer">
                         <Github size={20} />
                     </a>
-                    <a href="https://mz.linkedin.com/in/roan-yosky-timane-9902a0291" className="footer-link">
+                    <a href="https://mz.linkedin.com/in/roan-yosky-timane-9902a0291" className="footer-link" target="_blank" rel="noopener noreferrer">
                         <Linkedin size={20} />
                     </a>
                     <a href="mailto:roanyosky@gmail.com?subject=Contact Request&body=Hello, I would like to get in touch with you!" className="footer-link">
