b4167c812ec6c261a4013ad9881e7051ea5c3f68
Roan Yosky Timane <roanyosky@gmail.com>
2026-05-10 18:12:46 +0200

    Add Iris and Kinetic to the project section of the website

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,25 @@
 import palma from "../assets/images/palma.webp"
 import mozgnv from "../assets/images/mozgnv.webp"
+import kinetic from "../assets/images/kinetic.png"
+import iris from "../assets/images/iris.png"
 
 export const projects = [
+    {
+        title: 'Kinetic',
+        description: 'A web application for managing and tracking fitness activities',
+        tags: ['React', 'Spring Boot', 'PostgreSQL', 'Docker'],
+        status: 'completed',
+        image: kinetic,
+        link: 'https://kinetic.roanyosky.dev'
+    },
+    {
+        title: 'Iris',
+        description: 'A web application for uploading and editing images',
+        tags: ['React', 'Spring Boot', 'PostgreSQL', 'Docker'],
+        status: 'completed',
+        image: iris,
+        link: 'https://iris.roanyosky.dev'
+    },
     {
         title: 'Palma Empreendimentos',
         description: 'A portfolio website made with Vue js for Palma Empreendimentos',
